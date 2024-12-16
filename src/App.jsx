6326a44e9bf820296dde707acb4f28b7887ec688
Eric Grebe <eric.grebe@littlebottom.de>
2024-12-16 18:57:35 +0100

    Add wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Descripton from "./components/description";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Test Tailwind</h1>
+      <div class="flex flex-row flex-wrap py-4">
+        <aside class="w-full sm:w-1/3 md:w-1/4 px-2">
+          <div class="sticky top-0 p-4 w-full">
+            <ul class="flex flex-col overflow-hidden">
+              <Descripton />
+            </ul>
+          </div>
+        </aside>
+        <main role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2"></main>
+      </div>
+      <footer class="mt-auto">...</footer>
     </>
   );
 }
---
+++ src/components/description.jsx
@@ -0,0 +1,14 @@
+export default function Descripton() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Eric Grebe</h1>
+      <h2 className="font-bold">Software Developer</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus qui
+        itaque saepe officia nam, beatae pariatur unde veniam nobis veritatis,
+        velit quaerat consequuntur delectus optio quis, possimus porro deserunt
+        ipsa.
+      </p>
+    </div>
+  );
+}
