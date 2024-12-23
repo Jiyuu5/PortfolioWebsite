f754415be3525d1da8827ec601298f7ffe886e0f
Eric Grebe <jiyuu@rockbiter.de>
2024-12-23 12:03:39 +0100

    Change hover color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable react/prop-types */
 export default function Card({ title, description, tags, imgID, url }) {
   return (
-    <li className="mb-12 grid gap-4 grid-cols-3 sm:grid-cols-8">
-      <div className="sm:col-span-2 ">
-        <img
-          className="object-cover border-2 rounded border-gray-900"
-          src={`src/assets/projekt${imgID}.png`}
-        ></img>
-      </div>
-      <div className="col-span-2 sm:col-span-6">
-        <h2>
-          <a
-            href={url}
-            className="text-slate-100 text-base font-bold hover:text-teal-400"
-            target="_blank"
-          >
-            {title}
-          </a>
-        </h2>
-        <p className="text-sm leading-normal">{description}</p>
-        <div>{tags}</div>
-      </div>
+    <li>
+      <a href={url} target="_blank">
+        <div className="group mb-12 grid gap-4 grid-cols-3 sm:grid-cols-8 opacity-50 hover:opacity-100">
+          <div className="sm:col-span-2 ">
+            <img
+              className="object-cover border-2 rounded border-gray-900"
+              src={`src/assets/projekt${imgID}.png`}
+            ></img>
+          </div>
+          <div className="col-span-2 sm:col-span-6">
+            <h2>
+              <a
+                href={url}
+                className="text-slate-100 text-base font-bold group-hover:text-teal-400"
+                target="_blank"
+              >
+                {title}
+              </a>
+            </h2>
+            <p className="text-sm leading-normal">{description}</p>
+            <div>{tags}</div>
+          </div>
+        </div>
+      </a>
     </li>
   );
 }
--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -2,15 +2,15 @@
 export default function LanguageSelect({ handleSetLanguage }) {
   return (
     <ul className="flex gap-3 mt-1 mr-1">
-      <li>
+      <li className="hover:text-teal-500">
         <button onClick={() => handleSetLanguage("de")}>DE</button>
       </li>
       <li>|</li>
-      <li>
+      <li className="hover:text-teal-500">
         <button onClick={() => handleSetLanguage("en")}>EN</button>
       </li>
       <li>|</li>
-      <li>
+      <li className="hover:text-teal-500">
         <button onClick={() => handleSetLanguage("jp")}>JP</button>
       </li>
     </ul>
