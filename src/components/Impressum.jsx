58cd89143b1e09465726dd8bf5b8c6f0274f548f
Eric Grebe <grinsikp@littlebottom.de>
2024-12-24 13:09:08 +0100

    Change Impressum Style

--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -3,16 +3,18 @@ export default function Impressum({ isImpressumOpen }) {
   if (!isImpressumOpen) return;
 
   return (
-    <div className="">
-      <h2>Impressum</h2>
-      <p>
+    <div>
+      <h2 className="text-lg lg:text-xl font-bold mb-3">Impressum</h2>
+      <p className="mb-1">
         Eric Grebe <br />
         Hochgericht 8 <br />
         38640 Goslar
       </p>
       <p>
-        E-Mail: <a href="mailto:kontakt@ericgrebe.de"></a>
-        kontakt@ericgrebe.de
+        <span>E-Mail: </span>
+        <a className="hover:text-teal-500" href="mailto:kontakt@ericgrebe.de">
+          kontakt@ericgrebe.de
+        </a>
       </p>
     </div>
   );
