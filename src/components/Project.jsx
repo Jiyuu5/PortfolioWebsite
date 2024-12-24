dac848b4bdb538cf2cc92d97410347a7cb1efcb3
Eric Grebe <grinsikp@littlebottom.de>
2024-12-24 14:07:05 +0100

    Add link icon to Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,11 +11,27 @@ export default function Card({ title, description, tags, imgID, url }) {
             ></img>
           </div>
           <div className="col-span-2 sm:col-span-6">
-            <h2>
-              <span className="text-slate-100 text-base font-bold group-hover:text-teal-400">
-                {title}
-              </span>
-            </h2>
+            <div className="flex flex-row gap-2 items-end">
+              <h2>
+                <span className="text-slate-100 text-base font-bold group-hover:text-teal-400">
+                  {title}
+                </span>
+              </h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 group-hover:text-teal-400 group-hover:translate-x-2 group-hover:-rotate-45 transition-all"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </div>
             <p className="text-sm leading-normal">{description}</p>
             <ul className="flex flex-wrap gap-2 mt-2">
               {tags.map((item) => (
