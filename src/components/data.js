d72bd2d160f98f4224169ea21bb45f4351e14827
Eric Grebe <grinsikp@littlebottom.de>
2026-02-06 10:20:24 +0100

    Update description text

--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,7 @@ export const LANGUAGE_DATA = {
   de: {
     title: "Softwareentwickler",
     description:
-      "Herzlich willkommen auf meinem Profil! Momentan befinde ich mich in einer Umschulung zum Fachinformatiker für Anwendungsentwicklung und um Erfahrung zu sammeln, entwickle ich Projekte im Front-End-Bereich und Mobile-Apps für Android. In meiner Freizeit lerne ich Japanisch, um der Kultur Japans näher zu kommen.",
+      "Herzlich willkommen auf meinem Profil! Ich bin Fachinformatiker für Anwendungsentwicklung und um Erfahrung zu sammeln, entwickle ich Projekte im Front-End-Bereich und Mobile-Apps für Android. In meiner Freizeit lerne ich Japanisch, um der Kultur Japans näher zu kommen.",
     credit: [
       "Das Design dieser Website orientiert sich an der Portfolio-Site von",
       ". Vielen Dank für die beeindruckende Vorlage!",
@@ -11,7 +11,7 @@ export const LANGUAGE_DATA = {
   en: {
     title: "Software Developer",
     description:
-      "Welcome to my profile! I'm currently retraining as an IT specialist for application development and to gain experience I'm developing projects in the front-end area and mobile apps for Android. In my spare time I'm learning Japanese to get closer to the Japanese culture.",
+      "Welcome to my profile! I'm an IT specialist for application development and to gain experience I'm developing projects in the front-end area and mobile apps for Android. In my spare time I'm learning Japanese to get closer to the Japanese culture.",
     credit: [
       "The design of this website is based on the portfolio site of",
       ". Many thanks for the impressive template!",
@@ -20,7 +20,7 @@ export const LANGUAGE_DATA = {
   jp: {
     title: "アプリケーション開発のITスペシャリスト",
     description:
-      "私のプロフィールへようこそ！現在、アプリケーション開発のITスペシャリストとして再研修中で、経験を積むために、フロントエンド領域とアンドロイド向けモバイルアプリのプロジェクトを開発しています。余暇には日本の文化に近づくために日本語を勉強しています。",
+      "私のプロフィールへようこそ！私はアプリケーション開発のITスペシャリストで、経験を積むためにフロントエンド分野やAndroid向けモバイルアプリの開発に携わっています。余暇には、日本の文化をより深く理解するために日本語を学んでいます。",
     credit: [
       "このウェブサイトのデザインは、",
       "のポートフォリオサイトに基づいています。印象的なテンプレートに感謝します！",
